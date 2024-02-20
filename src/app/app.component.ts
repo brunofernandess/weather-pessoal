f87ae8627595d4d2310103f1b0eb91af331e46d6
brunofernandess <bruno_ffc@outlook.com>
2024-02-20 12:49:56 -0300

    weather pessoal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherFormComponent } from './page/home/weather-form/weather-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WeatherFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/page/home/weather-form/weather-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-form.component.html',
+  styleUrl: './weather-form.component.css'
+})
+export class WeatherFormComponent {
+
+}
